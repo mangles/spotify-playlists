83c2b7fc3e458391fef1436277a0767e5ff10f02
manglestw <mangles@thoughtworks.com>
2019-03-03 11:50:54 +0100

    Edit title of my app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <h1 >Ready to learn react?</h1>
+          <h1>My playlists</h1>
         <Aggregate/>
         <Aggregate/>
         <Filter/>
