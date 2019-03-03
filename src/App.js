cf959da4a7718bb21b96a1516c21996a5b46340a
manglestw <mangles@thoughtworks.com>
2019-03-03 11:15:55 +0100

    Mireia - First skeleton of the app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+let defaultTextColor = '#5F9EA0';
+let defaultStyle = {color: defaultTextColor};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <h1 >Ready to learn react?</h1>
+        <Aggregate/>
+        <Aggregate/>
+        <Filter/>
+        <Playlist/>
+        <Playlist/>
+        <Playlist/>
       </div>
+    );
+  }
+}
+
+class Aggregate extends Component {
+  render() {
+    return(
+        <div style={{...defaultStyle, width: '40%', display: 'inline-block'}}>
+          <h2>Number and text</h2>
+        </div>
+    );
+  }
+}
+
+class Filter extends Component{
+  render() {
+    return(
+        <div style={defaultStyle}>
+            <img/>
+          <input type='text'/> Filter
+        </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render() {
+    return(
+        <div style={{...defaultStyle, width: '25%', display: 'inline-block'}}>
+          <img/>
+          <h3>Playlist name</h3>
+          <ul><li>Top Songs</li>
+              <li>Top Songs</li>
+              <li>Top Songs</li>
+          </ul>
+        </div>
     );
   }
 }
